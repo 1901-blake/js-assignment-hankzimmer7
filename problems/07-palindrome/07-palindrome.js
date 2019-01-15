106c23b2c388f3e357d9884586ebf71946f22207
hankzimmer7 <hankzimmer7@gmail.com>
2019-01-15 13:21:13 -0500

    solves problem 7

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,20 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    let result = true;
+    for (let i = 0; i < someStr.length; i++) {
+        if (someStr[i] !== someStr[someStr.length-i-1]) {
+            result = false;
+        }
+    }
+    return result;
 }
+
+//Testing the function
+inputString = "racecar";
+palindromicity = isPalindrome(inputString);
+console.log(`Is ${inputString} a palindrome? ${palindromicity}`);
+
+inputString = "hello";
+palindromicity = isPalindrome(inputString);
+console.log(`Is ${inputString} a palindrome? ${palindromicity}`);
