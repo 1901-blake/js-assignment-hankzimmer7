43da521eab4a4c3122c9b40b2eca3f513c26aa87
hankzimmer7 <hankzimmer7@gmail.com>
2019-01-15 11:30:38 -0500

    adds comments to problem 2

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -4,10 +4,14 @@ Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
     let sortedArray = numArray;
+    //Initially assume the array is not sorted
     let arraySorted = false;
+    //Loop as long as the array isn't sorted
     while (!arraySorted) {
         arraySorted = true;
+        //Loop through the array
         for (let i = 0; i < sortedArray.length; i++) {
+            //If a value is great than the number to the right of it, swap the numbers and mark the array as not sorted
             if (sortedArray[i] > sortedArray[i + 1]) {
                 let temp = sortedArray[i + 1];
                 sortedArray[i + 1] = sortedArray[i];
