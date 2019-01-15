e6f4dd0b5c5f39bf228114147354875fb46d0f73
hankzimmer7 <hankzimmer7@gmail.com>
2019-01-15 15:37:51 -0500

    solves problem 8

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -20,6 +20,100 @@ Example for printShape("Diamond", 5, "*");
  ***
   * 
 */
+
 function printShape(shape, height, character) {
-  
+  switch (shape) {
+    case 'Square':
+      for (let i = 0; i < height; i++) {
+        let line = '';
+        for (let j = 0; j < height; j++) {
+          line += character;
+        }
+        console.log(line);
+      }
+      break;
+    case 'Triangle':
+      for (let i = 0; i < height; i++) {
+        let line = '';
+        for (let j = 0; j < height; j++) {
+          if (i >= j) {
+            line += character;
+          } else {
+            line += ' ';
+          }
+        }
+        console.log(line);
+      }
+      break;
+    case 'Diamond':
+      //Loop over rows
+      for (let i = 0; i < height; i++) {
+        let line = '';
+        //Loop over columns
+        for (let j = 0; j < height; j++) {
+          //Middle Lines
+          if (i === Math.floor(height / 2) || j === Math.floor(height / 2)) {
+            line += character;
+          }
+          //Upper Half
+          else if (i < height / 2) {
+            //Upper Left Corner
+            if (j < (height) / 2) {
+              if (j + i > (height) / 2 - 1) {
+                line += character;
+              } else {
+                line += ' ';
+              }
+            }
+            //Upper Right Corner
+            else if (j > (height) / 2) {
+              if (j < i + (height) / 2) {
+                line += character;
+              } else {
+                line += ' ';
+              }
+            }
+          }
+          //Lower Half
+          else {
+            //Lower Left Corner
+            if (j < (height) / 2) {
+              if (i - (height) / 2 <= j) {
+                line += character;
+              } else {
+                line += ' ';
+              }
+            }
+            //Lower Right Corner
+            if (j > (height) / 2) {
+              if (i + j <= 1.5 * (height) - 1) {
+                line += character;
+              } else {
+                line += ' ';
+              }
+            }
+          }
+        }
+        console.log(line);
+      }
+      break;
+    default:
+      break;
+  }
 }
+
+//Test the function
+let inputShape = 'Square';
+let inputHeight = 7;
+let intputCharacter = 'S';
+printShape(inputShape, inputHeight, intputCharacter);
+
+inputShape = 'Triangle';
+inputHeight = 8;
+intputCharacter = 'T';
+printShape(inputShape, inputHeight, intputCharacter);
+
+inputShape = 'Diamond';
+inputHeight = 9;
+intputCharacter = '*';
+printShape(inputShape, inputHeight, intputCharacter);
