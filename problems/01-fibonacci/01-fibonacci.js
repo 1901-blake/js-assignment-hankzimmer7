ffd19228ae876522c012e9628a12ac2ab505d634
hankzimmer7 <hankzimmer7@gmail.com>
2019-01-15 09:27:57 -0500

    solves problem 1

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,16 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    let currentNumber = 0;
+    let nextNumber = 1;
+    for ( let i = 0; i < n; i++) {
+        let temp = nextNumber;
+        nextNumber += currentNumber;
+        currentNumber = temp;
+    }
+    return currentNumber;
 }
+
+let position = 1000;
+let result = fib(position);
+console.log(`Number ${position} in the Fibonacci sequence is ${result}`);
